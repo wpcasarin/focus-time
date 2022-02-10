1d3bda9c107c0668cebf7da3402a6b9f6f075c08
wpcasarin <72427968+wpcasarin@users.noreply.github.com>
2022-02-10 10:52:33 -0300

    basic setup

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function App() {
+  const [focusSubject, setFocusSubject] = useState(null);
+  return <View style={styles.container}>{focusSubject ? null : null}</View>;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
